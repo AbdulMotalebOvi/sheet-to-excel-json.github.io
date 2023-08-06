c45f33de85baf42b57f11ddfeced58fdce312cfd
AbdulMotalebOvi <ovi425764@gmail.com>
2023-08-07 01:03:17 +0600

    server adding

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// right-1
 const express = require('express');
 const multer = require('multer');
 const ExcelJS = require('exceljs');
@@ -5,19 +6,30 @@ const cors = require('cors');
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage }).single('excelFile');
 
-const convertExcelToJson = async (excelBuffer, sheetName) => {
+const convertExcelToJson = async (excelBuffer, sheetIdentifier) => {
     const workbook = new ExcelJS.Workbook();
 
     try {
         await workbook.xlsx.load(excelBuffer);
-        const worksheet = workbook.getWorksheet(sheetName);
+
+        let worksheet;
+
+        if (typeof sheetIdentifier === 'number') {
+            worksheet = workbook.worksheets[sheetIdentifier];
+        } else {
+            worksheet = workbook.getWorksheet(sheetIdentifier);
+        }
+
+        if (!worksheet) {
+            console.log(`Worksheet '${sheetIdentifier}' not found.`);
+            return []; // Return an empty array or handle the error as needed
+        }
 
         const json = [];
 
@@ -45,8 +57,13 @@ app.post('/convert', upload, async (req, res) => {
         }
 
         const excelBuffer = req.file.buffer;
+        const sheetIdentifier = req.body.sheetIdentifier; // Access the sheet identifier from req.body
+        console.log(sheetIdentifier)
+        if (!sheetIdentifier) {
+            return res.status(400).send('Sheet identifier is missing.');
+        }
 
-        const jsonData = await convertExcelToJson(excelBuffer, 'Sheet1');
+        const jsonData = await convertExcelToJson(excelBuffer, sheetIdentifier);
 
         return res.status(200).json(jsonData);
     } catch (error) {
@@ -55,9 +72,15 @@ app.post('/convert', upload, async (req, res) => {
     }
 });
 
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-
-app.listen(5000, () => console.log('listening on port 5000...'));
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
